284ffb831887cf78e32c4b6ff5a430279671e587
ckkissane <67170576+ckkissane@users.noreply.github.com>
2020-07-14 20:06:12 -0400

    Add files via upload

---
+++ nutritionapp/forms.py
@@ -0,0 +1,100 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField,FieldList
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from nutritionapp.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    age = StringField('age',validators=[DataRequired()])
+    weight = StringField('weight')
+    height = StringField('height')
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    start_date = StringField('Start Date', validators=[DataRequired()])
+    body_weight = StringField('Body Weight', validators=[DataRequired()])
+    weekly_bw1 = StringField('BW')
+    weekly_bw2 = StringField('BW')
+    weekly_bw3 = StringField('BW')
+    weekly_bw4 = StringField('BW')
+    weekly_bw5 = StringField('BW')
+    weekly_bw6 = StringField('BW')
+    weekly_bw7 = StringField('BW')
+    weekly_bw8 = StringField('BW')
+    weekly_calories1 = StringField('Calories')
+    weekly_calories2 = StringField('Calories')
+    weekly_calories3 = StringField('Calories')
+    weekly_calories4 = StringField('Calories')
+    weekly_calories5 = StringField('Calories')
+    weekly_calories6 = StringField('Calories')
+    weekly_calories7 = StringField('Calories')
+    weekly_calories8 = StringField('Calories')
+    tdee = StringField('TDEE', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
